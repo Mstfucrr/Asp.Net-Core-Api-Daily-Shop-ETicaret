1c863b75bd30f45a58095e2728661099f699bfbf
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-04-15 18:02:11 +0300

    add home controller

---
+++ DailyShopETicaret.API/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyShopETicaret.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+	}
+}
